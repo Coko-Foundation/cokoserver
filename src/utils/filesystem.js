a2bc636605235180950e9a29e339ce560a4fbd99
Yannis Barlas <yannisbarlas@gmail.com>
2025-02-24 11:38:25 +0200

    feat(server): handle base64 data input in write file helper

--- src/utils/filesystem.js
+++ src/utils/filesystem.js
@@ -8,7 +8,22 @@ const tempFolderPath = require('./tempFolderPath')
 const writeFileToTemp = async (readStream, filePath) => {
   const outputPath = path.join(tempFolderPath, filePath)
   await fs.ensureFile(outputPath)
-  const dataBuffer = await buffer(readStream)
+
+  const isBase64 =
+    typeof readStream === 'string' &&
+    readStream.match(/[^:]\w+\/[\w\-+.]+(?=;base64,)/)
+
+  let dataBuffer
+
+  if (isBase64) {
+    dataBuffer = Buffer.from(
+      readStream.replace(/^data:.*;base64,/, ''),
+      'base64',
+    )
+  } else {
+    dataBuffer = await buffer(readStream)
+  }
+
   await fs.outputFile(outputPath, dataBuffer)
 }
 
