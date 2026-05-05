c8af220c396a3b2bc4442c6315c41eda068b30aa
yannis <yannisbarlas@gmail.com>
2026-05-05 14:14:56 +0300

    chore(server): remove redundant log when registering server components

--- packages/lib/src/startup/registerComponents.ts
+++ packages/lib/src/startup/registerComponents.ts
@@ -1,6 +1,5 @@
 import { Express } from 'express'
 import config from '../configManager/config'
-import logger from '../logger'
 import internalLogger from '../logger/internals'
 import loadComponent from '../utils/loadComponent'
 
@@ -11,13 +10,11 @@ const registerComponents = async (app: Express): Promise<void> => {
   await Promise.all(
     components.map(async (componentName: string) => {
       const component = await loadComponent(componentName)
-      internalLogger.success(`Registered component ${componentName}`)
-      const serverComponent = component.server
 
-      if (serverComponent) {
-        serverComponent()(app)
-        logger.info('Registered server component', componentName)
-      }
+      const serverComponent = component.server
+      if (serverComponent) serverComponent()(app)
+
+      internalLogger.success(`Registered component ${componentName}`)
     }),
   )
 }
