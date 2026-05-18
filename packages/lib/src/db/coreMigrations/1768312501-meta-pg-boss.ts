20a02635f0381b7397623479a2a8784146b3b5b8
yannis <yannisbarlas@gmail.com>
2026-05-18 15:16:38 +0300

    fix(db): make sure to await alter table statements in pg boss meta column migration

--- packages/lib/src/db/coreMigrations/1768312501-meta-pg-boss.ts
+++ packages/lib/src/db/coreMigrations/1768312501-meta-pg-boss.ts
@@ -4,13 +4,13 @@ const TABLE_NAME = 'coko_server_meta'
 const COLUMN_NAME = 'pg_boss_schema'
 
 export async function up(db: Knex): Promise<void> {
-  db.schema.alterTable(TABLE_NAME, t => {
+  await db.schema.alterTable(TABLE_NAME, t => {
     t.string(COLUMN_NAME)
   })
 }
 
 export async function down(db: Knex): Promise<void> {
-  db.schema.alterTable(TABLE_NAME, t => {
+  await db.schema.alterTable(TABLE_NAME, t => {
     t.dropColumn(COLUMN_NAME)
   })
 }
